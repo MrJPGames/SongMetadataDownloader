94b2448dc6c929a66382bbfb60ba25cd26f00fe5
Jasper W. D. M. Peters <jasper@fizazy.com>
2018-02-25 19:37:32 +0100

    Get data from mp3 file

--- downloader.php
+++ downloader.php
@@ -1,67 +1,92 @@
 <?php
 	error_reporting(E_ERROR);
+	require 'PHP-ID3/PhpId3/BinaryFileReader.php';
+	require 'PHP-ID3/PhpId3/Id3Tags.php';
+	require 'PHP-ID3/PhpId3/Id3TagsReader.php';
+	use PhpId3\Id3TagsReader;
 
-	unset($argv[0]);
-	$query = implode($argv, "+");
+	//unset($argv[0]);
+	//$query = implode($argv, "+");
 
-	$dURL = "https://genius.com/api/search/song?per_page=5&q=" . $query;
+	$filename = $argv[1];
+	if (file_exists($filename)){
+        $id3 = new Id3TagsReader(fopen(__DIR__ . "/". $filename, "rb"));
+        $id3->readAllTags();
 
-	$ch = curl_init($dURL);
+        foreach($id3->getId3Array() as $key => $value) {
+			if( $key !== "APIC" ) { //Skip Image data
+				echo $value["fullTagName"] . ": " . $value["body"] . PHP_EOL; 
+		    }else{
+		    	echo "Image";
+		    }
+		}
 
-    //return the transfer as a string 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		$id3Data = $id3->getId3Array();
 
-    $result = curl_exec($ch);  
+		$query = str_replace(" ", "+", $id3Data["TIT2"]["body"] . " " . $id3Data["TPE1"]["body"]);
 
-	$searchData = json_decode($result, true);
-	
-	if ($searchData["response"]["sections"][0]["hits"][0]["result"] != NULL){
-		$songData = $searchData["response"]["sections"][0]["hits"][0]["result"];
 
-		echo "Title: ", $songData["title"], PHP_EOL;
-		echo "Title with featured: ", $songData["title_with_featured"], PHP_EOL; 
-		echo "Artist: ", $songData["primary_artist"]["name"], PHP_EOL;
-		echo "Album art image URL: ", $songData["header_image_url"], PHP_EOL;
+		$dURL = "https://genius.com/api/search/song?per_page=5&q=" . $query;
 
-		$dURL = "https://genius.com/api" . $songData["api_path"];
+		$ch = curl_init($dURL);
 
-		echo "API URL: ", $songData["api_path"], PHP_EOL;
+	    //return the transfer as a string 
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 
-		curl_setopt($ch, CURLOPT_URL, $dURL);
+	    $result = curl_exec($ch);  
 
-	    $result = curl_exec($ch); 
+		$searchData = json_decode($result, true);
+		
+		if ($searchData["response"]["sections"][0]["hits"][0]["result"] != NULL){
+			$songData = $searchData["response"]["sections"][0]["hits"][0]["result"];
 
-		$jsonData = json_decode($result, true);
+			echo "Title: ", $songData["title"], PHP_EOL;
+			echo "Title with featured: ", $songData["title_with_featured"], PHP_EOL; 
+			echo "Artist: ", $songData["primary_artist"]["name"], PHP_EOL;
+			echo "Album art image URL: ", $songData["header_image_url"], PHP_EOL;
 
-		$songData = $jsonData["response"]["song"];
-		$albumData = $jsonData["response"]["song"]["album"];
+			$dURL = "https://genius.com/api" . $songData["api_path"];
 
-		echo "Full Title: ",  $songData["full_title"], PHP_EOL;
-		echo "Release date: ", $songData["release_date"], PHP_EOL;
-		echo "Recording Location: ", $songData["recording_location"], PHP_EOL;
-		echo "Album name: ", $albumData["name"], PHP_EOL;
-		echo "Lyrics page URL: ", $songData["path"];
+			echo "API URL: ", $songData["api_path"], PHP_EOL;
 
-		$dURL = "https://genius.com".$songData["path"];
+			curl_setopt($ch, CURLOPT_URL, $dURL);
 
-		curl_setopt($ch, CURLOPT_URL, $dURL);
+		    $result = curl_exec($ch); 
 
-		$result = curl_exec($ch);
+			$jsonData = json_decode($result, true);
 
-		$breaks = array("<br />","<br>","<br/>", "            ");  
-		$result = str_ireplace($breaks, "", $result);
+			$songData = $jsonData["response"]["song"];
+			$albumData = $jsonData["response"]["song"]["album"];
 
-		$HTMLdoc = new DOMDocument();
-		$HTMLdoc->loadHTML($result);
+			echo "Full Title: ",  $songData["full_title"], PHP_EOL;
+			echo "Release date: ", $songData["release_date"], PHP_EOL;
+			echo "Recording Location: ", $songData["recording_location"], PHP_EOL;
+			echo "Album name: ", $albumData["name"], PHP_EOL;
+			echo "Lyrics page URL: ", $songData["path"];
 
-		$xpath = new DomXpath($HTMLdoc);
+			$dURL = "https://genius.com".$songData["path"];
 
-		$rowNode = $xpath->query('//div[@class="lyrics"]')->item(0);
-		if ($rowNode instanceof DomElement) {
-		    echo $rowNode->nodeValue;
+			curl_setopt($ch, CURLOPT_URL, $dURL);
+
+			$result = curl_exec($ch);
+
+			$breaks = array("<br />","<br>","<br/>", "            ");  
+			$result = str_ireplace($breaks, "", $result);
+
+			$HTMLdoc = new DOMDocument();
+			$HTMLdoc->loadHTML($result);
+
+			$xpath = new DomXpath($HTMLdoc);
+
+			$rowNode = $xpath->query('//div[@class="lyrics"]')->item(0);
+			if ($rowNode instanceof DomElement) {
+			    echo $rowNode->nodeValue;
+			}
+		}else{
+			echo "[!] Did not find any song for " . $query, PHP_EOL;
 		}
 	}else{
-		echo "[!] Did not find any song for " . implode($argv," "), PHP_EOL;
+		echo "File not found!";
 	}
     curl_close($ch); 
 ?>
